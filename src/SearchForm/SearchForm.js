2e83df87ad49462ba4f490e457b941ee74d01f91
Paul Baisley <paulbaisley@Pauls-iMac-2282.home>
2020-02-08 18:26:47 -0500

    casing bug fix

--- src/SearchForm/SearchForm.js
+++ src/SearchForm/SearchForm.js
@@ -38,57 +38,63 @@ class SearchForm extends Component {
     });
 
     let foundPeople = [];
-    let {character, people} = this.state;
+    let { character, people } = this.state;
 
-    if (character.toLowerCase().includes('kuhn') || 
-        character.toLowerCase().includes('bennett')) {
+    if (
+      character.toLowerCase().includes('kuhn') ||
+      character.toLowerCase().includes('bennett')
+    ) {
       console.log(true);
       this.setState({
-        people: [{name: 'Bennett Kuhn', mass: '5000454500000'}],
+        people: [{ name: 'Bennett Kuhn', mass: '5000454500000' }],
         loading: false
-      })
+      });
       console.log(people);
-
     } else {
+      let testUrl = `https://swapi.co/api/people/?search=${character}`;
 
-    let testUrl = `https://swapi.co/api/people/?search=${character}`;
-
-    fetch(testUrl)
-    .then(res => {
-      if(!res.ok) {
-        throw new Error ('there was an error!')
-      }
-      return res.json();
-    })
-    .then(data => {
-      console.log(data.results);
-      if (data.results.length === 0) {
-        this.setState({
-          characterError: 'character not found!'
+      fetch(testUrl)
+        .then(res => {
+          if (!res.ok) {
+            throw new Error('there was an error!');
+          }
+          return res.json();
         })
-        console.log(this.state.characterError);
-      } else {
-      data.results.map(person => {
-        let casedPerson = person.name.toLowerCase();
-         if (casedPerson.includes(character)) {
-          return foundPeople.push({name: person.name, mass: person.mass });
-        }
-        return null;
-      })
+        .then(data => {
+          console.log(data.results);
+          if (data.results.length === 0) {
+            this.setState({
+              characterError: 'character not found!'
+            });
+            console.log(this.state.characterError);
+          } else {
+            data.results.map(person => {
+              let casedPersonLow = person.name.toLowerCase();
+              let casedPersonUp = person.name.toUpperCase();
+              if (person.name.includes(character) || 
+              casedPersonLow.includes(character) ||
+              casedPersonUp.includes(character)) {
+                return foundPeople.push({
+                  name: person.name,
+                  mass: person.mass
+                });
+              }
+              return null;
+            });
+          }
+          console.log(foundPeople);
+          return null;
+        })
+        .then(() => {
+          this.setState({
+            people: [...foundPeople],
+            loading: false,
+            character: ''
+          });
+        })
+        .catch(err => console.log(err.message));
     }
-        console.log(foundPeople);
-        return null;
-    })
-  .then(() => {
-    this.setState({
-      people: [...foundPeople],
-      loading: false,
-      character: ''
-    })
-  })
-  .catch(err => console.log(err.message))
-    }
-}
+  };
 
   handleChange = event => {
     console.log(event.target.value);
@@ -97,8 +103,6 @@ class SearchForm extends Component {
       nameError: '',
       characterError: ''
     });
-
-    
   };
 
   render() {
@@ -128,11 +132,21 @@ class SearchForm extends Component {
           ) : (
             <div>
               <ul>
-                {characterError ? 
-                <li> character not found!</li> : 
-                people.map((person, idx) => {
-                  return <li key={idx}>{person.name} weighs {person.mass > 0 ? Math.round((person.mass * 2.20462)) : '???'} lbs</li>;
-                })}
+                {characterError ? (
+                  <li> character not found!</li>
+                ) : (
+                  people.map((person, idx) => {
+                    return (
+                      <li key={idx}>
+                        {person.name} weighs{' '}
+                        {person.mass > 0
+                          ? Math.round(person.mass * 2.20462)
+                          : '???'}{' '}
+                        lbs
+                      </li>
+                    );
+                  })
+                )}
               </ul>
             </div>
           )}
